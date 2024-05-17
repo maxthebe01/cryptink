95e83d8c72ec18104961233be50f2afcb9ad11f9
maxthebe01 <114063571+maxthebe01@users.noreply.github.com>
2024-05-17 16:07:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function payWithPaystack(e) {
     let handler = PaystackPop.setup({
         key: 'pk_test_5975642a2e844677a780afd0dd877f6b81ee2577', // Replace with your public key
         email: document.getElementById("email-address").value,
-        amount: 70 * 100,
+        amount: 80 * 100,
         currency: 'ZAR',
         ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
         // label: "Optional string that replaces customer email"
