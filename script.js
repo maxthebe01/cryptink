15482e3a8f54750f2ae7c0e6585f1e80589c1230
maxthebe01 <114063571+maxthebe01@users.noreply.github.com>
2024-05-17 16:03:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,25 @@
 const paymentForm = document.getElementById('paymentForm');
-paymentForm.addEventListener('submit', payWithPaystack, false);
-const loadingIndicator = document.getElementById('loading');
+paymentForm.addEventListener("submit", payWithPaystack, false);
 
 function payWithPaystack(e) {
     e.preventDefault();
-    
-    // Show loading indicator and disable the button
-    payButton.disabled = true;
-    loadingIndicator.style.display = 'block';
-
+    const email = document.getElementById("email-address").value;
+    alert(email);
     let handler = PaystackPop.setup({
-        key: 'pk_test_5975642a2e844677a780afd0dd877f6b81ee2577', // Replace with your Paystack public key
-        email: document.getElementById('email').value,
-        amount: 80 * 100, // Amount in kobo
+        key: 'pk_test_5975642a2e844677a780afd0dd877f6b81ee2577', // Replace with your public key
+        email: document.getElementById("email-address").value,
+        amount: 70 * 100,
         currency: 'ZAR',
-        ref: '' + Math.floor(Math.random() * 1000000000 + 1), // Generates a random transaction reference
-        callback: function(response) {
-            alert('Payment successful. Transaction ref is ' + response.reference);
+        ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+        // label: "Optional string that replaces customer email"
+        onClose: function(){
+          alert('Window closed.');
+    },
+    callback: function(response){
+        let message = 'Payment complete! Reference: ' + response.reference;
+        alert(message);
+    }
+  });
 
-            // Hide loading indicator and enable the button
-            loadingIndicator.style.display = 'none';
-            payButton.disabled = false;
-        },
-        onClose: function() {
-            alert('Transaction was not completed, window closed.');
-
-            loadingIndicator.style.display = 'none';
-            payButton.disabled = false;
-        }
-    });
     handler.openIframe();
-
-    loadingIndicator.style.display = 'block'; // Show the loading indicator
-
-    // Simulate form submission delay (e.g., process payment)
-    setTimeout(function () {
-        // Here you would typically submit the form data using AJAX or proceed with your payment processing logic.
-        paymentForm.submit(); // Submit the form (or handle it via AJAX)
-    }, 2000); // Simulate a 2-second delay
 }
